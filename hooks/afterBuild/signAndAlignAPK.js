89137c0daeb6297fa107e542034f0c318c9600ae
Andrew Hite <andrew@andrew-hite.com>
2016-08-24 10:30:55 -0500

    Add hook to sign and align release apks

---
+++ hooks/afterBuild/signAndAlignAPK.js
@@ -0,0 +1,53 @@
+var fs = require('fs');
+var path = require('path');
+var paths = require('../config/paths');
+var childProcess = require('child_process');
+
+function signApkFile(fileName) {
+  try {
+    childProcess.execSync(
+      `jarsigner
+        -verbose
+        -sigalg SHA1withRSA
+        -digestalg SHA1
+        -keystore ${config.path}
+        ${paths.appDist}/${fileName}
+        ${config.alias}
+      `
+    );
+  } catch(error) {
+    console.error(error);
+  }
+}
+
+function alignApkFile(fileName) {
+  try {
+    var signedFileName = fileName.replace(/release/, 'release-signed');
+
+    childProcess.execSync(
+      `zipalign
+        -v 4
+        ${paths.appDist}/${fileName}
+        ${paths.appDist}/${signedFileName}
+      `
+    );
+  } catch(error) {
+    console.error(error);
+  }
+}
+
+module.exports = function(context) {
+  if (fs.existsSync(paths.appKeystore)) {
+    var config = JSON.parse(fs.readFileSync(paths.appKeystore));
+    var apkFiles = fs.readdirSync(paths.appDist);
+
+    apkFiles.forEach(function(apkFileName) {
+      if (apkFileName.match(/release/)) {
+        signApkFile(apkFileName);
+        alignApkFile(apkFileName);
+      }
+    });
+  } else {
+    console.warn('Could not sign and align APK. No keystore.json found.');
+  }
+}
